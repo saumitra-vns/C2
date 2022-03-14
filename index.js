b829b48dc3a98e9fa45b3091a957126b3f80d443
saumitra-vns <97459938+saumitra-vns@users.noreply.github.com>
2022-03-14 12:15:32 +0530

    start

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+
+
+app.listen(5500, async () =>{
+
+    console.log("listining on port 5500")
+})
